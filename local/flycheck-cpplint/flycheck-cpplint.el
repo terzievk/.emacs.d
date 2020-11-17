2c6bcecae3bfe12d4fffecc47b19324254321072
Konstantin Terziev <terzievk@yahoo.com>
2020-11-17 21:30:42 +0200

    fix flycheck-cpplint bug caused by flymake temp files

--- local/flycheck-cpplint/flycheck-cpplint.el
+++ local/flycheck-cpplint/flycheck-cpplint.el
@@ -7,14 +7,16 @@
 (require 'google-c-style)
 
 (flycheck-define-checker cpplint
-    "A C/C++ syntax checker using cpplint.
+  "A C/C++ syntax checker using cpplint.
 
-See URL `https://github.com/google/styleguide'."
-    :command ("cpplint" source)
-    :error-patterns
-    ((error line-start (file-name) ":" line ":" (message) line-end))
-    :modes (c-mode c++-mode)
-    :next-checkers ((warning . google-c-style)))
+See URL `https://github.com/cpplint/cpplint'."
+  ;; source-inplace instead of source because cpplint gets confused
+  ;; by the flycheck temp files and prints wrong header guard messages
+  :command ("cpplint" source-inplace)
+  :error-patterns
+  ((error line-start (file-name) ":" line ":" (message) line-end))
+  :modes (c-mode c++-mode)
+  :next-checkers ((warning . google-c-style)))
 
 (add-to-list 'flycheck-checkers 'cpplint)
 
