2c4a8b7e566e4f9ec5b10493df3c0ad00b96c4f7
Konstantin Terziev <terzievk@yahoo.com>
2024-02-03 11:19:24 +0200

    keep up with minor daily changes

--- init.el
+++ init.el
@@ -147,6 +147,57 @@
   :config
   (setq magit-view-git-manual-method 'man))
 
+;; ;; helm: Emacs incremental completion and selection narrowing framework
+;; (use-package helm
+;;   :config
+;;   ;;; extended config from: https://tuhdo.github.io/helm-intro.html
+
+;;   ;; helm-config replaced with helm-autoloads
+;;   ;;  https://github.com/emacs-helm/helm/commit/e81fbbc687705595ab65ae5cd3bdf93c17a90743
+
+;;   ;;  (require 'helm-config)
+;;   (require 'helm-autoloads)
+
+;;   ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;;   ;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;;   ;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+;;   (global-set-key (kbd "C-c h") 'helm-command-prefix)
+;;   (global-unset-key (kbd "C-x c"))
+
+;;   (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+;;   (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+;;   (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+;;   (when (executable-find "curl")
+;;     (setq helm-google-suggest-use-curl-p t))
+
+;;   (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+;;         helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+;;         helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+;;         helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+;;         helm-ff-file-name-history-use-recentf t
+;;         helm-echo-input-in-header-line t)
+
+;;   (defun spacemacs//helm-hide-minibuffer-maybe ()
+;;     "Hide minibuffer in Helm session if we use the header line as input field."
+;;     (when (with-helm-buffer helm-echo-input-in-header-line)
+;;       (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+;;         (overlay-put ov 'window (selected-window))
+;;         (overlay-put ov 'face
+;;                      (let ((bg-color (face-background 'default nil)))
+;;                        `(:background ,bg-color :foreground ,bg-color)))
+;;         (setq-local cursor-type nil))))
+
+
+;;   (add-hook 'helm-minibuffer-set-up-hook
+;;             'spacemacs//helm-hide-minibuffer-maybe)
+
+;;   (setq helm-autoresize-max-height 0)
+;;   (setq helm-autoresize-min-height 20)
+;;   (helm-autoresize-mode 1)
+
+;;   (helm-mode 1))
+
 ;; zenburn-theme: The Zenburn colour theme ported to Emacs
 (use-package zenburn-theme
   :ensure t
@@ -307,9 +358,15 @@ See URL `https://github.com/cpplint/cpplint'."
                         (file-exists-p "Makefile"))
               (set (make-local-variable 'compile-command)
                    ;; name execs f for ease of use
-                   (concat "g++ -Wall -Werror -std=c++20 "
+                   (concat "g++ "
                            buffer-file-name
-                           " -o f")))))
+                           " -o f"
+                           " -std=c++2b"
+                           ;; some learncpp.com suggested options
+                           " -O2 -DNDEBUG"
+                           " -pedantic-errors"
+                           " -Wall -Weffc++ -Wextra -Wconversion -Wsign-conversion"
+                           " -Werror")))))
 
 
 ;; save all buffers for me before compilation
@@ -457,6 +514,7 @@ See 'compilation-finish-functions to for the arguments:  BUF STR."
   ;; (web-mode-code-indent-offset 2)
   :config
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
   ;; to do: read some elisp regex...
   ;;  (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
 
@@ -519,7 +577,7 @@ Save, allign and open `localhost/project`."
   (interactive)
   (lsp-format-buffer)
   (save-buffer)
-  (browse-url-chrome "localhost/project"))
+  (browse-url-chrome "127.0.0.1"))
 
 ;; swift: https://github.com/emacs-lsp/lsp-sourcekit
 ;; competion doesn't work, see:  https://github.com/emacs-lsp/lsp-mode/issues/3028
